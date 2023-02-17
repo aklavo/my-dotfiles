f125314b14c032ca3cddd583933015850534dfea
Andrew Klavekoske <77470325+aklavo@users.noreply.github.com>
2023-02-16 22:00:39 -0700

    Update useful-commands.sh

--- useful-commands.sh
+++ useful-commands.sh
@@ -1,8 +1,23 @@
+# oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
 # zsh pullin sources
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# Terminal color theme = Screry
+# p10k themes
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+# link files to cloned repo files
+ln -s workspace/MyLinuxEnv/.zshrc .zshrc
+ln -s workspace/MyLinuxEnv/.p10k.zsh .p10k.zsh
+
+# reload configurations
+exec zsh
+
+# Terminal color theme = Screry (206)
 bash -c  "$(wget -qO- https://git.io/vQgMr)" 
 
-# Terminal font = MesloLGS NF
+# Terminal font = MesloLGS NF (set in terminal preferences)
+sudo bash -c "$(curl -LSs https://github.com/dfmgr/installer/raw/main/install.sh)"
+sudo fontmgr install MesloLGSNF
